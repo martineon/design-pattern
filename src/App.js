c0abc7c6802acd1e833f3c5d4967f39b36215c3b
nitramnoe <eon.martin@icloud.com>
2017-04-20 12:03:39 +0200

    essai

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './Components/Menu/Menu.jsx';
-import Notif from './Components/Notif/Notif.jsx';
+// import Menu from './Components/Menu/Menu.jsx';
+// import Notif from './Components/Notif/Notif.jsx';
 import Carousel from './Components/Carousel/Carousel.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Notif/>
-        <Menu/>
+        {/* <Notif/>
+        <Menu/> */}
         <Carousel/>
       </div>
     );
