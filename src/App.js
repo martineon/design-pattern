c80aa5e1cda4bf9268848987ac17503fd61bee78
Moraru Ion <morarou.y@gmail.com>
2017-04-20 16:25:57 +0200

    Add component notify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Menu from './Components/Menu/Menu.jsx';
-// import Notif from './Components/Notif/Notif.jsx';
+import Notif from './Components/Notif/Notif.jsx';
 import Carousel from './Components/Carousel/Carousel.jsx';
 
 class App extends Component {
